ff5a24cb2503a03de3ef057beef07b0f4b1e5ea0
Steven <liang1058@126.com>
2022-09-16 10:21:06 +0800

    update

--- py2cpp/dtw.cpp
+++ py2cpp/dtw.cpp
@@ -9,24 +9,17 @@ double dtw_distance(const std::vector<double>& x, const std::vector<double>& y) 
     const int nx = static_cast<int>(x.size());
     const int ny = static_cast<int>(y.size());
 
-    std::vector<std::vector<double>> d(nx, std::vector<double>(ny));
+    // Initialize all dtw matrix elements to infinity
+    std::vector<std::vector<double>> d(nx + 1, std::vector<double>(ny + 1, INFINITY));
+    d[0][0] = 0;
     double dist;
 
-    // Initialize all dtw matrix elements to infnty
-    for (int i = 0; i < nx; ++i) {
-        d[i][0] = INFINITY;
-    }
-    for (int i = 0; i < ny; ++i) {
-        d[0][i] = INFINITY;
-    }
-    d[0][0] = 0;
-
     // Determine the optimal dtw path via dynamic programming,
-    for (int i = 1; i < nx; i++) {
-        for (int j = 1; j < ny; j++) {
-            dist = std::abs(x[i] - y[j]);
+    for (int i = 1; i <= nx; i++) {
+        for (int j = 1; j <= ny; j++) {
+            dist = std::abs(x[i - 1] - y[j - 1]);
             d[i][j] = dist + std::min({ d[i - 1][j], d[i][j - 1], d[i - 1][j - 1] });
         }
     }
-    return d[nx - 1][ny - 1];
+    return d[nx][ny];
 }
